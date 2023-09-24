54f23d0bfa0fc0f1052123b56d46948ac5f8a007
Vitor augusto demattos <vitoraugusto2010201078@gmail.com>
2023-09-24 08:55:01 -0300

    created Header

---
+++ portfolio/pages/components/Header.tsx
@@ -0,0 +1,18 @@
+import { BiMoon } from "react-icons/bi";
+export function Header() {
+  return (
+  <>
+     <header className="flex container mx-auto text-black h-20 ">
+       <div className="flex items-center justify-between w-4/5  mx-auto p-4">
+              <h1 className="text-3xl">Vitor Augusto</h1>
+              <div className="border-2 h-12 w-12 flex justify-center items-center rounded-full cursor-pointer 
+              bg-whitemoon
+              ">
+                <BiMoon size={25} />
+                
+                </div>
+        </div>
+     </header>
+  </>
+  );
+}
--- portfolio/tailwind.config.ts
+++ portfolio/tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: "#FF9C1A",
+        secondary: "#EC1B09",
+        tertiary: "#F2F5F9",
+        whitemoon: "#EBF2FA",
+        black: "#000000",
+      }
     },
   },
   plugins: [],
